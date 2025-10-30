e3c918b8cc3776387f08384e93dc1b084d482122
joynto7 <joyntoghosh7@gmail.com>
2025-10-30 14:02:53 +0600

    Using cd command

---
+++ cd.sh
@@ -0,0 +1,13 @@
+# change directory 
+echo "cd stand for change directory"
+
+#General Usage 
+echo "cd directory_name (to change to a specific directory)"
+
+echo "cd .. (to move one level up in the directory)" 
+
+echo "cd ~ (to go to the home directoy)"
+
+echo "cd - (to switch to the previous directory)"
+
+echo "cd / (to go to the root directoruy)"
