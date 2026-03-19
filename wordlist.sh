653961ee4fc47d952e088eabdd28dcc6d45106d8
joynto7 <joyntoghosh7@gmail.com>
2026-03-19 23:41:01 +0600

    adding a new txt file

---
+++ wordlist.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+# Simple Wordlist Generator
+# Generates common username/password combinations
+
+read -rp "Enter a name : " NAME
+read -rp "Enter a number: " NUM
+
+echo ""
+echo " <<<------ Generate a worlist ---->>>
+echo "$NAME"
+echo "$NAME$NUM"
+echo "$NAME@$NUM"
+echo "$NAME#$NUM"
+echo "${NAME}123"
+echo "${NAME}2024"
+echo "${NAME}2025"
+echo "${NAME}_admin"
+echo "admin_${NAME}"
+echo "admin"
+echo "password"
+echo "123456"
+echo "${NAME}password"
+echo "password${NUM}"
+
+echo ""
+echo "--- Saving to wordlist.txt ---"
+
+
