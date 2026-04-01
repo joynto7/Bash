e424a74beeb605c400834707e9c19eff1a2600a0
joyntoghosh <joyntoghosh7@gmail.com>
2026-04-01 23:59:52 +0600

    the second part

--- ip_info.sh
+++ ip_info.sh
@@ -8,3 +8,8 @@ read -rp "Enter an IP address: " IP
 echo ""
 echo "--- IP Info for $IP ---"
 echo ""
+# ipinfo.io is a free public API that returns IP details as JSON
+RESULT=$(curl -s "https://ipinfo.io/$IP/json")
+ 
+# Parse each field from the JSON response
+echo "  IP       : $(echo "$RESULT" | grep '"ip"'       | cut -d'"' -f4)"
